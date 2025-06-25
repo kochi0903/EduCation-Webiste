9944f6e59433eb095601a135f03dd389069c7160
Ishita Dey <ishitadey.222@gmail.com>
2025-06-25 22:23:03 +0530

    Redesigned the Register Page

--- src/components/layout/Register.jsx
+++ src/components/layout/Register.jsx
@@ -24,13 +24,13 @@ const Register = () => {
       newErrors.email = 'Email address is invalid.';
     }
 
-    if(!phoneNumber.trim()){
-      newErrors.phoneNumber= "Phone number is required.";
-    }else if(phoneNumber.length!==10){
-      newErrors.phoneNumber="Phone number is invalid";
+    if (!phoneNumber.trim()) {
+      newErrors.phoneNumber = "Phone number is required.";
+    } else if (phoneNumber.length !== 10) {
+      newErrors.phoneNumber = "Phone number is invalid";
     }
 
-     if (!password) {
+    if (!password) {
       newErrors.password = 'Password is required.';
     } else if (password.length < 6) {
       newErrors.password = 'Password must be at least 6 characters.';
@@ -38,7 +38,7 @@ const Register = () => {
 
     setErrors(newErrors);
 
-    if (Object.keys(newErrors).length===0){
+    if (Object.keys(newErrors).length === 0) {
       console.log("Registered");
       // setIsRegistered(true);
       setFullName('');
@@ -48,76 +48,76 @@ const Register = () => {
     }
   };
 
-  console.log("Errors:",errors)
+  console.log("Errors:", errors)
 
   return (
     <>
-      
-        <div className="registerContainer bg-gradient-to-b from-slate-300 to-slate-100 flex justify-evenly items-center pt-10 space-x-3 relative">
-          <div className="left h-[35rem] w-1/2 flex justify-center items-center">
-            <div className="pic w-full h-full flex items-center justify-center">
-              <img
-                className='w-full h-full object-contain object-center rounded-[3rem]'
-                src="src/assets/Register.png"
-                alt="Group Meeting"
-              />
-            </div>
-          </div>
-          <div className="right w-[45rem] flex flex-col justify-center items-center p-3">
-            <div className="account text-3xl font-bold  mb-2">Join With Us</div>
-            <form onSubmit={handleSubmit}>
 
+      <div className="registerContainer  flex justify-evenly items-center pt-10 space-x-3 relative bg-[url('src/assets/background.png')] bg-cover bg-no-repeat bg-[center_40%]">
+        <div className="left h-[35rem] w-1/2 flex justify-center items-center">
+          <div className="pic w-full h-full flex items-center justify-center">
+            {/* <img
+              className='w-full h-full object-contain object-center rounded-[3rem]'
+              src="src/assets/Register.png"
+              alt="Group Meeting"
+            /> */}
+          </div>
+        </div>
+        <div className="right w-[33rem] h-[25rem] rounded-2xl flex flex-col justify-center items-center p-3  bg-gray-300 bg-opacity-50 border-2 border-gray-600">
+          <div className="account text-3xl font-bold  mb-2">Join With Us</div>
+          <form onSubmit={handleSubmit}>
+            <div className="top flex space-x-10 mt-2">
               {/* Name Field */}
               <div className={`name flex flex-col mt-3`}>Full Name
                 <input type="text"
                   placeholder='Ishita Dey'
                   name="fullname"
-                  className={`p-1 mt-3 rounded-xl border  hover:shadow-lg transition-shadow duration-200 ${
-                  errors.fullName ? 'border-red-500' : 'border-transparent'
-                }  text-base pl-3
-                hover:border-gray-400`}
+                  className={`p-1 mt-3 rounded-sm border  hover:shadow-lg transition-shadow duration-200 ${errors.fullName ? 'border-red-500' : 'border-transparent'
+                    }  text-base pl-3
+                hover:border-gray-600`}
                   value={fullName}
-                  onChange={(e)=> setFullName(e.target.value)}
-                   />
-                   {errors.fullName &&(
-                    <p className='text-red-400 text-xs mt-1 mb-0'>{errors.fullName}</p>
-                   )}
+                  onChange={(e) => setFullName(e.target.value)}
+                />
+                {errors.fullName && (
+                  <p className='text-red-400 text-xs mt-1 mb-0'>{errors.fullName}</p>
+                )}
               </div>
 
               {/* Email Field */}
               <div className="email flex flex-col mt-3">Email
                 <input type="email"
                   placeholder='user@example.com'
-                  className={`p-1 mt-3 rounded-xl pl-3 border
-                     hover:shadow-lg transition-shadow duration-200 ${
-                  errors.email ? 'border-red-500' : 'border-transparent'
-                }
-                hover:border-gray-400`} 
-                value={email}
-                onChange={(e)=> setEmail(e.target.value)}
+                  className={`p-1 mt-3 rounded-sm pl-3 border
+                     hover:shadow-xl shadow-gray-600 transition-shadow duration-200 ${errors.email ? 'border-red-500' : 'border-transparent'
+                    }
+                hover:border-gray-600`}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
-                {errors.email &&(
-                    <p className='text-red-400 text-xs mt-1 mb-0'>{errors.email}</p>
-                 )}
+                {errors.email && (
+                  <p className='text-red-400 text-xs mt-1 mb-0'>{errors.email}</p>
+                )}
 
               </div>
 
+
+            </div>
+            <div className="bottom flex space-x-10 mt-3">
               {/* Phone Number */}
               <div className="phone flex flex-col mt-3">Phone Number
                 <input type="tel"
                   placeholder='91*******90'
-                  className={`p-1 mt-3 rounded-xl pl-3 border
+                  className={`p-1 mt-3 rounded-sm pl-3 border
                     hover:shadow-lg transition-shadow duration-200
-                  ${
-                    errors.phoneNumber ? 'border-red-500' : 'border-transparent'
-                  }
-                  hover:border-gray-400`}
+                  ${errors.phoneNumber ? 'border-red-500' : 'border-transparent'
+                    }
+                  hover:border-gray-600`}
                   value={phoneNumber}
-                  onChange={(e)=> setPhoneNumber(e.target.value)}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
                 />
-                {errors.phoneNumber &&(
-                    <p className='text-red-400 text-xs mt-1 mb-0'>{errors.phoneNumber}</p>
-                 )}
+                {errors.phoneNumber && (
+                  <p className='text-red-400 text-xs mt-1 mb-0'>{errors.phoneNumber}</p>
+                )}
 
               </div>
 
@@ -125,29 +125,32 @@ const Register = () => {
               <div className="password flex flex-col mt-3">Password
                 <input type="password"
                   placeholder='******'
-                  className={`p-1 mt-3 rounded-xl pl-3 border
+                  className={`p-1 mt-3 rounded-sm pl-3 border
                      hover:shadow-lg transition-shadow duration-200
-                  ${
-                    errors.password ? 'border-red-500' : 'border-transparent'
-                  }
-                  hover:border-gray-400`}
+                  ${errors.password ? 'border-red-500' : 'border-transparent'
+                    }
+                  hover:border-gray-600`}
                   value={password}
-                  onChange={(e)=>setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
-                {errors.password &&(
-                    <p className='text-red-400 text-xs mt-1 mb-0'>{errors.password}</p>
-                 )}
+                {errors.password && (
+                  <p className='text-red-400 text-xs mt-1 mb-0'>{errors.password}</p>
+                )}
               </div>
+            </div>
 
-
-              <div className="submit mt-12 p-3 bg-yellow-400 flex justify-center align-center rounded-full">
-                <input type="submit" />
-              </div>
-            </form>
-          </div>
-         
+            <div className="submit flex justify-center items-center">
+              <input type="submit"
+              className=' mt-12 w-60 h-10 bg-yellow-400 flex justify-center align-center rounded-full 
+              hover:bg-yellow-500
+              active:bg-yellow-300
+              active:border border-yellow-600' />
+            </div>
+          </form>
         </div>
-      
+
+      </div>
+
     </>
   )
 }
